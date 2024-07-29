a4cc9b28cb6d0744e8b0fcb078fa7fc4a7b29789
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-07-29 13:30:36 +0700

    [Add] Page Add Product

--- Front-End/src/App.js
+++ Front-End/src/App.js
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProductList from "./component/ProductList";
+import AddProduct from "./component/AddProduct";
 function App() {
   return (
     <div className='container'>
       <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<ProductList />} />
-      </Routes>
+        <Routes>
+          <Route path='/' element={<ProductList />} />
+          <Route path='/add' element={<AddProduct />} />
+        </Routes>
       </BrowserRouter>
     </div>
   );
