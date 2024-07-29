65468b22e7d5c42fa03ff11067441f8c8822be7c
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-07-29 09:14:17 +0700

    add basic routes for product

---
+++ controllers/ProductController.js
@@ -0,0 +1,19 @@
+export const getProducts = (req, res) => {
+
+}
+
+export const getProductById = (req, res) => {
+
+}
+
+export const createProducts = (req, res) => {
+
+}
+
+export const updateProducts = (req, res) => {
+
+}
+
+export const deleteProducts = (req, res) => {
+
+}
